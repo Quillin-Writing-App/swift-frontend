23fbaa18aa860ec877cb2c2db5d586231147d87f
ottokunkel <ottokunkel034@gmail.com>
2025-02-08 13:43:00 -0500

    Initial Commit

---
+++ writing-app/writing_appApp.swift
@@ -0,0 +1,18 @@
+//
+//  writing_appApp.swift
+//  writing-app
+//
+//  Created by Otto Kunkel on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct writing_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            // test
+        }
+    }
+}
